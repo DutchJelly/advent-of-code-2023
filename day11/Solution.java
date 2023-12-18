f0466fb2aba36e464521a20f752d31283531dd60
dutchjelly <jelle.keulemans@gmail.com>
2023-12-18 20:37:50 +0100

    day 11

--- day11/Solution.java
+++ day11/Solution.java
@@ -1,4 +1,4 @@
-package day4;
+package day11;
 
 import java.io.*;
 import java.util.*;
@@ -7,14 +7,71 @@ import java.nio.file.*;
 
 public class Solution {
 
+  private static record Galaxy(long i, long j) {
+  }
+
+  private static List<Galaxy> parseGalaxies(List<String> input, int multiplier) {
+    List<Galaxy> galaxies = new ArrayList<>();
+    List<Integer> emptyRows = new ArrayList<>();
+    List<Integer> emptyColumns = new ArrayList<>();
+    for (int i = 0; i < input.size(); i++) {
+      if (!input.get(i).contains("#")) {
+        emptyRows.add(i);
+      }
+    }
+    for (int j = 0; j < input.get(0).length(); j++) {
+      boolean emptyColumn = true;
+      for (int i = 0; i < input.size(); i++) {
+        if (input.get(i).charAt(j) == '#') {
+          emptyColumn = false;
+          break;
+        }
+      }
+      if (emptyColumn) {
+        emptyColumns.add(j);
+      }
+    }
+    for (int i = 0; i < input.size(); i++) {
+      for (int j = 0; j < input.get(i).length(); j++) {
+        if (input.get(i).charAt(j) == '#') {
+          final int ii = i;
+          final int jj = j;
+          galaxies.add(new Galaxy(i + (emptyRows.stream().filter(x -> x < ii).count() * multiplier),
+              j + (emptyColumns.stream().filter(x -> x < jj).count() * multiplier)));
+        }
+      }
+    }
+    return galaxies;
+  }
+
+  private static long getDistance(Galaxy a, Galaxy b) {
+    return Math.abs(a.i - b.i) + Math.abs(a.j - b.j);
+  }
+
   private static void part1(List<String> input) throws Exception {
     int result = 0;
+
+    List<Galaxy> galaxies = parseGalaxies(input, 1);
+
+    for (int i = 0; i < galaxies.size(); i++) {
+      for (int j = i + 1; j < galaxies.size(); j++) {
+        result += getDistance(galaxies.get(i), galaxies.get(j));
+      }
+    }
 
     System.out.println("part 1: %d".formatted(result));
   }
 
   private static void part2(List<String> input) {
-    int result = 0;
+    long result = 0;
+
+    List<Galaxy> galaxies = parseGalaxies(input, 1_000_000 - 1);
+
+    for (int i = 0; i < galaxies.size(); i++) {
+      for (int j = i + 1; j < galaxies.size(); j++) {
+        result += getDistance(galaxies.get(i), galaxies.get(j));
+      }
+    }
 
     System.out.println("part 2: %d".formatted(result));
   }
